b86d333b2af1d84b6011786a80567afc399c861b
Muhammad Ahmad <muhammadahmadbwp@hotmail.com>
2024-08-23 04:20:40 +0500

    datetime fields add to the post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.utils import timezone
+# from django.db.models.functions import Now
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    
+    publish = models.DateTimeField(default=timezone.now)
+    # publish = models.DateTimeField(db_default=Now())          # Django 5 feature: uses the database server’s current date and time as the default
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
